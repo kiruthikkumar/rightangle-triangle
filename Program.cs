b16b53e18049e6d11f36a62ac88e19df6c3286fd
kiruthikkumar <83568693+kiruthikkumar@users.noreply.github.com>
2021-10-28 14:22:27 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace rightangle_triangle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i;
+            Console.Write("Enter the value : ");
+            i = Convert.ToInt32(Console.ReadLine());
+            for(int n=i;n>0;n--)
+            {
+                Console.WriteLine(" ");
+                for (int j=0;j<n;j++)
+                {
+                    Console.Write(" * ");
+                }
+                
+            }
+            Console.WriteLine(" ");
+        }
+    }
+}
